d2f10c63e76ee5fb4c30b8af688c72d07cbbc095
Joschka Eickhoff <jeickhoff@posteo.de>
2023-01-26 00:39:59 +0100

    basic nav button positioning

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,39 @@
 import styled from 'styled-components';
 
+console.clear();
+
 const StyledNav = styled.nav`
-	background-color: lightgoldenrodyellow;
+	display: grid;
+	align-items: center;
+	justify-content: space-around;
 	position: absolute;
 	bottom: 0;
-	min-height: 9%;
-	width: 90%;
+	width: 90vw;
+	min-height: 60px;
+	background-color: lightgoldenrodyellow;
+`;
+const StyledUl = styled.ul`
+	display: grid;
+	grid-template-columns: repeat(3, 1fr);
+	width: inherit;
+`;
+const StyledLi = styled.li`
+	display: grid;
+	justify-content: center;
+	margin: 0 20px;
+	padding: 5px 0;
+	border: 1px solid black;
+	border-radius: 20px;
 `;
 
 export default function Footer() {
 	return (
 		<StyledNav>
-			<div>© 2022 by Josh</div>
+			<StyledUl>
+				<StyledLi>🏠</StyledLi>
+				<StyledLi>📂</StyledLi>
+				<StyledLi>🧸</StyledLi>
+			</StyledUl>
 		</StyledNav>
 	);
 }
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,10 +6,8 @@ import Header from './Header.jsx';
 const StyledMain = styled.main`
 	display: grid;
 	grid-template-columns: 1fr;
-	width: 375px;
 	min-height: calc(100vh - 160px);
 	background-color: orange;
-	font-size: ;
 `;
 
 export default function Layout({children}) {
