eced48c216cc1096ca1724f9490efecc6f092a27
Joschka Eickhoff <jeickhoff@posteo.de>
2023-03-15 15:51:18 +0100

    code uncommented

--- src/pages/allVocabs/[id].js
+++ src/pages/allVocabs/[id].js
@@ -1,32 +1,32 @@
-// import {getAllFlashcards, getVocabById} from '../../services/vocabServices';
+import {getAllFlashcards, getVocabById} from '../../services/vocabServices';
 
-// export async function getStaticPaths() {
-// 	const allVocabs = await getAllFlashcards();
-// 	return {
-// 		paths: allVocabs.map(vocab => ({
-// 			params: {
-// 				id: vocab.front,
-// 			},
-// 		})),
-// 		fallback: false,
-// 	};
-// }
+export async function getStaticPaths() {
+	const allVocabs = await getAllFlashcards();
+	return {
+		paths: allVocabs.map(vocab => ({
+			params: {
+				id: vocab.front,
+			},
+		})),
+		fallback: false,
+	};
+}
 
-// export const getStaticProps = context => {
-// 	const vocab = getVocabById(context.params.id);
-// 	return {
-// 		props: {vocab},
-// 	};
-// };
+export const getStaticProps = context => {
+	const vocab = getVocabById(context.params.id);
+	return {
+		props: {vocab},
+	};
+};
 
-// export default function AllVocabs({vocab}) {
-// 	const {front, back} = vocab;
+export default function AllVocabs({vocab}) {
+	const {front, back} = vocab;
 
-// 	return (
-// 		<>
-// 			{/* <p>{'id: ' + id}</p> */}
-// 			<p>{'front => ' + front}</p>
-// 			<p>{'back => ' + back}</p>
-// 		</>
-// 	);
-// }
+	return (
+		<>
+			{/* <p>{'id: ' + id}</p> */}
+			<p>{'front => ' + front}</p>
+			<p>{'back => ' + back}</p>
+		</>
+	);
+}
