3f103f35fa88723daaf6052bd3b8d3b546d13d51
Joschka Eickhoff <jeickhoff@posteo.de>
2023-03-28 01:45:44 +0200

    DELETED Button Link 'StartLearning'

--- src/components/Button/StartLearning.jsx
+++
@@ -1,9 +0,0 @@
-import Button from './Button';
-
-export default function StartLearning() {
-	return (
-		<Button href="/session" onClick={() => console.log('test')} variant={'startLearning'}>
-			Study
-		</Button>
-	);
-}
