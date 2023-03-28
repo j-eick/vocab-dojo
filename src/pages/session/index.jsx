e94af96b04d6b001a0f81dd373b75e9ed9a0842d
Joschka Eickhoff <jeickhoff@posteo.de>
2023-03-28 02:21:55 +0200

    ADDED: getStaticProps to sessionPage + hardcoding one Card into page

--- src/pages/session/index.jsx
+++ src/pages/session/index.jsx
@@ -1,3 +1,25 @@
-export default function StudySession() {
-	return <p>hi</p>;
+import FlashCard from '../../components/Flashcard';
+import Layout from '../../components/Layout';
+import {getAllVocabs} from '../../services/vocabServices';
+
+console.clear();
+
+export const getStaticProps = () => {
+	const allVocabs = getAllVocabs();
+
+	return {
+		props: {
+			allVocabs,
+		},
+	};
+};
+
+export default function StudySession({allVocabs}) {
+	console.log(allVocabs[0].front);
+
+	return (
+		<Layout>
+			<FlashCard front={allVocabs[0].front} back={allVocabs[0].back} />
+		</Layout>
+	);
 }
