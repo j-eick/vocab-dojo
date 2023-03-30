47330574e4984cd7d0f3fa851f4b065f79183b94
Joschka Eickhoff <jeickhoff@posteo.de>
2023-03-30 14:07:17 +0200

    moved Styles to bottom of file

--- src/components/Form/AddVocabForm.jsx
+++ src/components/Form/AddVocabForm.jsx
@@ -4,64 +4,6 @@ import {vocabStore} from '../../hooks/useStore';
 
 console.clear();
 
-const Wrapper = styled.div`
-	width: 100%;
-
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-`;
-const StyledCardUl = styled.ul`
-	width: 80%;
-	padding: 20px;
-	margin-top: 50px;
-
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	gap: 20px;
-
-	background-color: yellow;
-`;
-const StyledCardLi = styled.li`
-	display: flex;
-	flex-direction: row;
-	justify-content: center;
-
-	gap: 10px;
-
-	border: 1px solid black;
-`;
-const StyledCardSide = styled.p`
-	position: relative;
-	width: 120px;
-
-	display: flex;
-	justify-content: center;
-
-	/* margin: 20px; */
-	padding: 20px;
-	background-color: #62aed4;
-`;
-
-const Trash = styled.span`
-	position: absolute;
-	top: -7px;
-	right: -5px;
-	font-size: 1.5rem;
-`;
-
-const StyledForm = styled.form`
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-`;
-const StyledSubmit = styled.input.attrs({
-	type: 'submit',
-})`
-	width: 40%;
-`;
-
 export default function AddVocabForm() {
 	const getVocabList = vocabStore(state => state.vocabList);
 	const addVocabs = vocabStore(state => state.addVocabs);
@@ -120,3 +62,61 @@ export default function AddVocabForm() {
 		</Wrapper>
 	);
 }
+
+const Wrapper = styled.div`
+	width: 100%;
+
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+`;
+const StyledCardUl = styled.ul`
+	width: 80%;
+	padding: 20px;
+	margin-top: 50px;
+
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	gap: 20px;
+
+	background-color: yellow;
+`;
+const StyledCardLi = styled.li`
+	display: flex;
+	flex-direction: row;
+	justify-content: center;
+
+	gap: 10px;
+
+	border: 1px solid black;
+`;
+const StyledCardSide = styled.p`
+	position: relative;
+	width: 120px;
+
+	display: flex;
+	justify-content: center;
+
+	/* margin: 20px; */
+	padding: 20px;
+	background-color: #62aed4;
+`;
+
+const Trash = styled.span`
+	position: absolute;
+	top: -7px;
+	right: -5px;
+	font-size: 1.5rem;
+`;
+
+const StyledForm = styled.form`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+`;
+const StyledSubmit = styled.input.attrs({
+	type: 'submit',
+})`
+	width: 40%;
+`;
