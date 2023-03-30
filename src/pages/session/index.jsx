47465649314ac3af2dc7fc5d441c3fb49edcf944
Joschka Eickhoff <jeickhoff@posteo.de>
2023-03-30 14:08:56 +0200

    RESET component to functioning state; pullRAndomCard() still not solvd

--- src/pages/session/index.jsx
+++ src/pages/session/index.jsx
@@ -1,6 +1,3 @@
-import {useEffect} from 'react';
-import {useState} from 'react';
-
 import NextFlashCard from '../../components/Button/NextFlashCard';
 import FlashCard from '../../components/Flashcard';
 import Layout from '../../components/Layout';
@@ -11,39 +8,22 @@ console.clear();
 export const getStaticProps = () => {
 	const allVocabs = getAllVocabs();
 
-	function pullRandomCard() {
-		const floatCard = Math.random(allVocabs.length);
-		const randomizedCard = Math.round(floatCard * 4);
-		return randomizedCard;
-	}
-
-	const initialVocab = allVocabs[pullRandomCard()];
-	console.log(initialVocab);
-
 	return {
-		props: {allVocabs, initialVocab},
+		props: {allVocabs},
 	};
 };
 
-export default function StudySession({allVocabs, initialVocab}) {
-	const [currentCard, setCurrentCard] = useState(initialVocab);
-
-	console.log(currentCard);
-
-	useEffect(() => {
-		setCurrentCard(pullRandomCard());
-	}, []);
-
-	function pullRandomCard() {
-		const floatCard = Math.random(allVocabs.length);
-		const randomizedCard = Math.round(floatCard * 4);
-		setCurrentCard(allVocabs[randomizedCard]);
-	}
+export default function StudySession() {
+	// function getRandomArrayNum() {
+	// 	const floatCard = Math.random(allVocabs.length);
+	// 	const randomizedCard = Math.round(floatCard * 4);
+	// 	return randomizedCard;
+	// }
 
 	return (
 		<Layout>
-			<FlashCard front={currentCard.front} back={currentCard.back} />
-			<NextFlashCard onClick={pullRandomCard} />
+			<FlashCard />
+			<NextFlashCard />
 		</Layout>
 	);
 }
