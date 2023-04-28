d41077afd491bd246c2660c1d63181acec92b4aa
Joschka Eickhoff <jeickhoff@posteo.de>
2023-04-28 14:51:24 +0200

    cleaned up

--- src/components/Links/StartSession.jsx
+++ src/components/Links/StartSession.jsx
@@ -1,18 +1,9 @@
-// import {Zap} from '@styled-icons/fa-solid/Brain';
-
 import LinkTemplate from '.';
 
 export default function StartSession({variant}) {
 	return (
 		<LinkTemplate href="/session" variant={variant}>
-			<p>
-				Lets go
-				{/* <RedZap /> */}
-			</p>
+			<div>Lets go</div>
 		</LinkTemplate>
 	);
 }
-
-// const RedZap = styled(Zap)`
-// 	color: red;
-// `;
