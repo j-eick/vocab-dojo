80f4dc9bdc3f620fe8aacc8a18e970c5a82480f4
Joschka Eickhoff <jeickhoff@posteo.de>
2023-04-28 12:50:41 +0200

    working on 'startSession'-link: need to install styled-icons

--- src/components/Links/StartSession.jsx
+++ src/components/Links/StartSession.jsx
@@ -1,9 +1,18 @@
+// import {Zap} from '@styled-icons/fa-solid/Brain';
+
 import LinkTemplate from '.';
 
-export default function StartSession() {
+export default function StartSession({variant}) {
 	return (
-		<LinkTemplate href="/session" variant={'startSession'}>
-			Start
+		<LinkTemplate href="/session" variant={variant}>
+			<p>
+				Lets go
+				{/* <RedZap /> */}
+			</p>
 		</LinkTemplate>
 	);
 }
+
+// const RedZap = styled(Zap)`
+// 	color: red;
+// `;
