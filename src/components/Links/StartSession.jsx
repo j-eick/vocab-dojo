a3e60f29b045bf22ca107fd31913c76e0bdcfb24
Joschka Eickhoff <jeickhoff@posteo.de>
2023-04-28 15:35:41 +0200

    MV StartSession StartSessionLink

--- src/components/Links/StartSession.jsx
+++
@@ -1,9 +0,0 @@
-import LinkTemplate from '.';
-
-export default function StartSession({variant}) {
-	return (
-		<LinkTemplate href="/session" variant={variant}>
-			<div>Lets go</div>
-		</LinkTemplate>
-	);
-}
---
+++ src/components/Links/StartSessionLink.jsx
@@ -0,0 +1,28 @@
+import {mdiCards} from '@mdi/js';
+import Icon from '@mdi/react';
+import styled from 'styled-components';
+
+import LinkTemplate from '.';
+
+export default function StartSessionLink({variant}) {
+	return (
+		<LinkTemplate href="/session" variant={variant}>
+			<StyledDiv>
+				<StyledP>Start Session</StyledP>
+				<Icon path={mdiCards} size={2} />
+			</StyledDiv>
+		</LinkTemplate>
+	);
+}
+
+const StyledDiv = styled.div`
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	justify-content: space-around;
+`;
+
+const StyledP = styled.p`
+	padding-right: 10px;
+	font-size: var(--fontsize_ButtonText);
+`;
