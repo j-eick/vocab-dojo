05ed3abb55fc3df33dc46cb2ae3cb80ae08ada02
Joschka Eickhoff <jeickhoff@posteo.de>
2023-04-29 16:36:14 +0200

    CREATED CreateFlashcardButton component based off of re-usable Button component

---
+++ src/components/Button/CreateFlashcardButton.jsx
@@ -0,0 +1,9 @@
+import Button from './Button';
+
+export default function CreateFlashcardButton() {
+	return (
+		<Button type="submit" name="submit" variant="createFlashcard">
+			New Flashcard
+		</Button>
+	);
+}
