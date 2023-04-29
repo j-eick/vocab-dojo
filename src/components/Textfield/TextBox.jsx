0c0d2f7a95090f5fe80fceb2b15caad4aef98ff4
Joschka Eickhoff <jeickhoff@posteo.de>
2023-04-30 01:31:55 +0200

    minor changes

--- src/components/Textfield/TextBox.jsx
+++
@@ -1,5 +0,0 @@
-import Textfield from './index';
-
-export default function TextBox({children, variant}) {
-	return <Textfield variant={variant}>{children}</Textfield>;
-}
--- src/components/Textfield/TextFieldMain.jsx
+++
@@ -1,5 +0,0 @@
-import Textfield from './index';
-
-export default function TextfieldMainPage({children, variant}) {
-	return <Textfield variant={variant}>{children}</Textfield>;
-}
