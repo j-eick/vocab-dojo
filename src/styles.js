ef2c55965f52d7fbad80388c41976991da1bb3ec
Joschka Eickhoff <jeickhoff@posteo.de>
2023-04-30 01:13:54 +0200

    CHANGED  size of 'fontsize_Textfield_title'

--- src/styles.js
+++ src/styles.js
@@ -4,7 +4,7 @@ export const GlobalStyle = createGlobalStyle`
 
 	:root {
 		--fontsize_HeaderTitle: 2rem;
-		--fontsize_Textfield_title: 2.5rem;
+		--fontsize_Textfield_title: 2rem;
 		--fontsize_Textfield_text: 1.75rem;
 		--fontsize_Textfield_createVocab: 1rem;
 		--fontsize_defaultText: 1.5rem;
