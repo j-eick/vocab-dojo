f198fd8d3bf35872975ffcb4bf3591d834277bf0
Joschka Eickhoff <jeickhoff@posteo.de>
2023-05-01 16:26:29 +0200

    created: shuffleCards() function

--- src/pages/session/index.jsx
+++ src/pages/session/index.jsx
@@ -9,21 +9,50 @@ import {getRandomNum} from '../../utils/functions';
 console.clear();
 
 export async function getStaticProps() {
-	const startingCard = getRandomNum(4);
 	const allFlashcards = await getAllFlashcards();
+	const sumAllFlashcards = await allFlashcards.length;
+	const startingCard = await getRandomNum(sumAllFlashcards);
+	const newArray = shuffleCards();
+
+	/**
+	 * Exporting this function to src/utils/functions results in the following error:
+	 *
+	 * 	TypeError: can't access property "Flashcard", mongoose__WEBPACK_IMPORTED_MODULE_0___default().models is undefined
+	 */
+	function shuffleCards() {
+		// 1. Make copy of card array + create empty array
+		const origArray = allFlashcards;
+		const newArray = [];
+
+		while (origArray.length != 0) {
+			// 1.0. get origArray length, generate random integer
+			//  	and use that number to choose card x
+			const numOfCards = origArray.length;
+			const randomNum = Math.floor(Math.random() * numOfCards);
+			const cardForNewArray = origArray[randomNum];
+
+			// 1.1. Paste card x into new array
+			newArray.push(cardForNewArray);
+			// 1.2. Cut card x out of orig array
+			origArray.splice(randomNum, 1);
+		}
+
+		// 2. Return new array
+		return newArray;
+	}
 
 	return {
-		props: {startingCard, allFlashcards},
+		props: {startingCard, newArray},
 	};
 }
 
-export default function StudySession({startingCard, allFlashcards}) {
-	const [currentCard, setCurrentCard] = useState(allFlashcards[startingCard]);
+export default function StudySession({startingCard, newArray}) {
+	const [currentCard, setCurrentCard] = useState(newArray[startingCard]);
 
 	function getRandomCard() {
-		const sumAllFlashcards = allFlashcards.length;
+		const sumAllFlashcards = newArray.length;
 		const randomNum = getRandomNum(sumAllFlashcards);
-		return allFlashcards[randomNum];
+		return newArray[randomNum];
 	}
 
 	return (
