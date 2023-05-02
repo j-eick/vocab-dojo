cd599cdfbfb83e4c2b4e76dbbc3f2288eb7edec5
Joschka Eickhoff <jeickhoff@posteo.de>
2023-05-02 16:00:42 +0200

    ADDED inputField variant

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,7 +1,16 @@
 import styled, {css} from 'styled-components';
 
-export default function Inputfield({id, text, name, onChange, variant}) {
-	return <StyledInput id={id} type={text} name={name} onChange={onChange} variant={variant} />;
+export default function Inputfield({id, text, name, onChange, variant, placeholder}) {
+	return (
+		<StyledInput
+			id={id}
+			type={text}
+			name={name}
+			onChange={onChange}
+			variant={variant}
+			placeholder={placeholder}
+		/>
+	);
 }
 
 const StyledInput = styled.input`
@@ -24,6 +33,43 @@ const StyledInput = styled.input`
 				rgb(209, 213, 219) 0px 0px 0px 1px inset;
 			&:focus {
 				background: #e6e8ea;
+			}
+		`}
+
+	${({variant}) =>
+		variant === 'input_createVocabs_title' &&
+		css`
+			width: 80%;
+			height: 3em;
+
+			margin-bottom: 10px;
+			padding: 8px;
+
+			outline: none;
+			border: 1px solid white;
+			border-radius: 10px;
+			font-size: var(--fontsize_Textfield_text);
+			line-break: normal;
+
+			background: #dae2eb;
+
+			box-shadow: rgba(0, 0, 0, 0.05) 0px 0px 0px 1px,
+				rgb(209, 213, 219) 0px 0px 0px 1px inset;
+
+			/* IE: default scrollbar is hidden  */
+			overflow: auto;
+			&:focus {
+				background: #e6e8ea;
+
+				&::placeholder {
+					color: transparent;
+				}
+			}
+			&::placeholder {
+				color: gray;
+				font-size: var(--fontsize_Placeholder_MultilineInput);
+				opacity: 0.3;
+				text-align: left;
 			}
 		`}
 `;
