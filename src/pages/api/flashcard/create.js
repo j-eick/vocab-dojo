f14531c47b95467f8c0c3ad6bbb220d6456d8580
Joschka Eickhoff <jeickhoff@posteo.de>
2023-05-07 22:57:28 +0200

    cleaned up

--- src/pages/api/flashcard/create.js
+++ src/pages/api/flashcard/create.js
@@ -9,13 +9,13 @@ export default async function handler(req, res) {
 	if (req.method === 'POST') {
 		const data = JSON.parse(req.body);
 
-		console.log('after parse: ' + data);
-
 		await Flashcard.create(data);
 		res.status(200).json({message: 'Flashcard created'});
 	} // ++++  DELETE  ++++
 	// else if (req.method === 'DELETE') {
+	// 	console.log('req.method: ' + 'DELETE');
 	// 	// code
-	// 	await Flashcard.findOneAndDelete(id);
+	// 	await Flashcard.deleteMany({});
 	// 	res.status(200).json({message: 'Flashcard deleted'});
+	// }
 }
