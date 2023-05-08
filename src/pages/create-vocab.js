d5e0bc33527f3523027e0199fa2d9179dc4f957c
Joschka Eickhoff <jeickhoff@posteo.de>
2023-05-08 15:18:38 +0200

    added vocab is sent to globalState as well

--- src/pages/create-vocab.js
+++ src/pages/create-vocab.js
@@ -1,24 +1,19 @@
 import AddVocabForm from '../components/Form/AddVocabForm';
 import Layout from '../components/Layout';
 import {useFetch} from '../hooks/useFetch';
-// import {getAllFlashcards} from '../../services/vocabServices';
-
-// export async function getServerSideProps() {
-// 	const flashcards = await getAllFlashcards();
-
-// 	return {
-// 		props: {flashcards: flashcards},
-// 	};
-// }
+import {vocabStore} from '../hooks/useStore';
 
 export default function CreateVocab() {
 	const fetchAPI = useFetch();
+	// const mainList = vocabStore(state => state.mainList);
+	const addVocab = vocabStore(state => state.addVocab);
 
 	async function handleSubmit(data) {
 		await fetchAPI('/api/flashcard/create', {
 			method: 'POST',
 			body: JSON.stringify(data),
 		});
+		addVocab(data);
 	}
 
 	return (
