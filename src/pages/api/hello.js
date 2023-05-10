0ffc3bb274e69d1e3a3144a779e2d603662b1242
Joschka Eickhoff <jeickhoff@posteo.de>
2023-05-10 14:23:10 +0200

    testing chatgpt response; ERROR 401

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,3 +1,18 @@
+import {openai} from '../../utils/openai';
+
 export default async function handler(request, response) {
-	response.status(200).json({name: 'John Doe'});
+	const completion = await openai.createCompletion({
+		model: 'text-davinci-003',
+		prompt: 'Hello world',
+	});
+	response.status(200).json(completion.data.choices[0].text);
+
+	// const completion = await openai.createCompletion({
+	// 	model: 'text-davinci-003',
+	// 	prompt: 'what is 2 + 2?',
+	// });
+
+	// response.status(200).json(completion.data.choices[0].text);
+
+	// receive user data, ask chat gpt to generate somethign, and return to the user
 }
