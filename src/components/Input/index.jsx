768745da5e7da858b11b8c64d5f7e9941c197391
Joschka Eickhoff <jeickhoff@posteo.de>
2023-05-12 21:44:23 +0200

    minor changes

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -15,6 +15,48 @@ export default function Inputfield({id, text, name, onChange, variant, placehold
 
 const StyledInput = styled.input`
 	${({variant}) =>
+		variant === 'input_createVocabs_title' &&
+		css`
+			width: 80%;
+			height: 2em;
+
+			margin-bottom: 4px;
+			padding: 8px;
+
+			outline: none;
+			border: 1px solid whitesmoke;
+			border-radius: 7px;
+			/* border-top-right-radius: 10px;
+			border-top-left-radius: 10px; */
+			font-size: var(--fontsize_Textfield_text);
+			line-break: normal;
+
+			background: #dae2eb;
+
+			box-shadow: rgba(0, 0, 0, 0.05) 0px 0px 0px 1px,
+				rgb(209, 213, 219) 0px 0px 0px 1px inset;
+
+			/* opacity: 0; */
+
+			/* IE: default scrollbar is hidden  */
+			overflow: auto;
+			&:focus {
+				background: #e6e8ea;
+
+				&::placeholder {
+					color: transparent;
+				}
+			}
+			&::placeholder {
+				color: gray;
+				font-size: var(--fontsize_Flashcard_title_placeholder);
+				font-style: italic;
+				opacity: 0.3;
+				text-align: left;
+			}
+		`}
+
+	${({variant}) =>
 		variant === 'input_createVocabs' &&
 		css`
 			margin-bottom: 20px;
@@ -33,43 +75,6 @@ const StyledInput = styled.input`
 				rgb(209, 213, 219) 0px 0px 0px 1px inset;
 			&:focus {
 				background: #e6e8ea;
-			}
-		`}
-
-	${({variant}) =>
-		variant === 'input_createVocabs_title' &&
-		css`
-			width: 80%;
-			height: 3em;
-
-			margin-bottom: 10px;
-			padding: 8px;
-
-			outline: none;
-			border: 1px solid white;
-			border-radius: 10px;
-			font-size: var(--fontsize_Textfield_text);
-			line-break: normal;
-
-			background: #dae2eb;
-
-			box-shadow: rgba(0, 0, 0, 0.05) 0px 0px 0px 1px,
-				rgb(209, 213, 219) 0px 0px 0px 1px inset;
-
-			/* IE: default scrollbar is hidden  */
-			overflow: auto;
-			&:focus {
-				background: #e6e8ea;
-
-				&::placeholder {
-					color: transparent;
-				}
-			}
-			&::placeholder {
-				color: gray;
-				font-size: var(--fontsize_Placeholder_MultilineInput);
-				opacity: 0.3;
-				text-align: left;
 			}
 		`}
 `;
