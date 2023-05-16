1c711accbcf5e72c856d3bee6a4b4094f2333e3f
Joschka Eickhoff <jeickhoff@posteo.de>
2023-05-16 12:42:34 +0200

    ADDED conditional rendering

--- src/pages/session/index.jsx
+++ src/pages/session/index.jsx
@@ -3,6 +3,7 @@ import {useState} from 'react';
 import NextFlashCardButton from '../../components/Button/NextFlashCard';
 import FlashCard from '../../components/Flashcard';
 import Layout from '../../components/Layout';
+import PreSessionModal from '../../components/Modals/PreSessionModal';
 import {getAllFlashcards} from '../../services/vocabServices';
 import {getRandomNum} from '../../utils/functions';
 import {shuffleCards} from '../../utils/functions';
@@ -23,6 +24,8 @@ export async function getStaticProps() {
 
 export default function StudySession({startingCard, newArray}) {
 	const [currentCard, setCurrentCard] = useState(newArray[startingCard]);
+	const [showPreSessionModal, setShowPreSessionModal] = useState(true);
+	const [showNextCardButton, setShowNextCardButton] = useState(false);
 
 	console.log('currentCard');
 	console.log(currentCard);
@@ -33,15 +36,23 @@ export default function StudySession({startingCard, newArray}) {
 		return newArray[randomNum];
 	}
 
+	function clickHandler() {
+		setShowPreSessionModal(!showPreSessionModal);
+		setShowNextCardButton(!showNextCardButton);
+	}
+
 	return (
 		<Layout>
+			{showPreSessionModal && <PreSessionModal onClick={clickHandler} />}
 			<FlashCard
 				frontTitle={currentCard.frontTitle}
 				frontDescription={currentCard.frontDescription}
 				backTitle={currentCard.backTitle}
 				backDescription={currentCard.backDescription}
 			/>
-			<NextFlashCardButton onClick={() => setCurrentCard(getRandomCard())} />
+			{showNextCardButton && (
+				<NextFlashCardButton onClick={() => setCurrentCard(getRandomCard())} />
+			)}
 		</Layout>
 	);
 }
