793db116d5069c62ed01ee455d115a294f6fb1f9
Joschka Eickhoff <jeickhoff@posteo.de>
2023-05-20 02:17:25 +0200

    Conditional SessionLink

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,18 @@
 import Head from 'next/head';
 
 import Layout from '../components/Layout';
-import StartSession from '../components/Links/StartSessionLink';
+import StartSession_noCards from '../components/Links/StartSessionLink_noCards';
+import StartSession_withCards from '../components/Links/StartSessionLink_withCards';
 import Textfield from '../components/Texfield/Textbox';
 import TextfieldArea from '../components/TextfieldArea';
 import Title from '../components/Title/index';
+import {useToggleStore} from '../hooks/useToggleStore';
+
+console.clear();
 
 export default function HomePage() {
+	const cardsInList = useToggleStore(state => state.cardsInList);
+
 	return (
 		<Layout>
 			<Head>
@@ -27,7 +33,13 @@ export default function HomePage() {
 
 			{/* #############   START SESSION   ################ */}
 			<Title variant="Textbox_Title">Train your brain</Title>
-			<StartSession variant="startSession" />
+
+			{/* PICK DIFFEREN STARTSESSION BUTTON BASED ON CONDITION, whether CardList is full or empty */}
+			{cardsInList ? (
+				<StartSession_withCards variant="startSession_withCards" />
+			) : (
+				<StartSession_noCards variant="startSession_noCards" />
+			)}
 		</Layout>
 	);
 }
