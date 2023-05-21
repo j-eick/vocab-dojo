157bce1005d1a3dccf236097d3819874a91229a8
Joschka Eickhoff <jeickhoff@posteo.de>
2023-05-21 16:00:04 +0200

    added "showModal" toggle to toggleStore

--- src/hooks/useToggleStore.js
+++ src/hooks/useToggleStore.js
@@ -2,9 +2,12 @@ import create from 'zustand';
 
 const toggleStore = set => ({
 	cardsInList: true,
-
 	toggleNoCards: () => set({cardsInList: false}),
 	toggleWithCards: () => set({cardsInList: true}),
+
+	showModal: true,
+	hide_Modal: () => set({showModal: false}),
+	show_Modal: () => set({showModal: true}),
 });
 
 export const useToggleStore = create(toggleStore);
