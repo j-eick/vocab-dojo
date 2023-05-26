2fbaabbd3e635df79765c228e65fb767ccaefc8e
Joschka Eickhoff <jeickhoff@posteo.de>
2023-05-26 13:18:44 +0200

    added background structure; added border-radius

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -8,8 +8,8 @@ const Canvas = styled.div`
 	place-items: center;
 	width: 100vw;
 	min-height: 100vh;
-	background: rgb(234, 240, 247);
-	background: linear-gradient(207deg, rgba(234, 240, 247, 1) 0%, rgba(212, 223, 234, 1) 57%);
+	background-color: #c1d6ec;
+	background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='28' height='49' viewBox='0 0 28 49'%3E%3Cg fill-rule='evenodd'%3E%3Cg id='hexagons' fill='%23d4e4f1' fill-opacity='0.3' fill-rule='nonzero'%3E%3Cpath d='M13.99 9.25l13 7.5v15l-13 7.5L1 31.75v-15l12.99-7.5zM3 17.9v12.7l10.99 6.34 11-6.35V17.9l-11-6.34L3 17.9zM0 15l12.98-7.5V0h-2v6.35L0 12.69v2.3zm0 18.5L12.98 41v8h-2v-6.85L0 35.81v-2.3zM15 0v7.5L27.99 15H28v-2.31h-.01L17 6.35V0h-2zm0 49v-8l12.99-7.5H28v2.31h-.01L17 42.15V49h-2z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
 `;
 
 const ResponsiveContainer = styled.div`
@@ -20,7 +20,6 @@ const ResponsiveContainer = styled.div`
 	@media (min-width: 376px) {
 		width: 375px;
 		min-height: 667px;
-		border: 1px solid black;
 	}
 `;
 
@@ -31,6 +30,18 @@ const StyledMain = styled.main`
 	height: calc(667px - 60px);
 	background: rgb(221, 231, 244);
 	background: linear-gradient(55deg, rgba(221, 231, 244, 1) 0%, rgba(191, 206, 226, 1) 10%);
+
+	@media (min-width: 376px) {
+		border-top-right-radius: 35px;
+		border-top-left-radius: 35px;
+		/* border-top: 0.5px solid gray;
+		border-right: 0.5px solid gray;
+		border-left: 0.5px solid gray; */
+
+		@media (min-width: 500px), (min-height: 750px) {
+			box-shadow: rgba(0, 0, 0, 0.3) 0px 10px 38px, rgba(0, 0, 0, 0.22) 0px 15px 12px;
+		}
+	}
 `;
 
 export default function Layout({children}) {
