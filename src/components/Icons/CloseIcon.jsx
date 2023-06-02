f0b7a5c25979344563d238d3a1175b2d87e6350c
Joschka Eickhoff <jeickhoff@posteo.de>
2023-06-02 16:05:58 +0200

    ADDED close icon

--- src/components/Icons/CloseIcon.jsx
+++ src/components/Icons/CloseIcon.jsx
@@ -3,9 +3,9 @@ import Icon from '@mdi/react';
 
 import IconTemplate from '.';
 
-export default function CloseIcon({size, onClick}) {
+export default function CloseIcon({size, onClick, variant}) {
 	return (
-		<IconTemplate>
+		<IconTemplate variant={variant}>
 			<Icon path={mdiClose} size={size} onClick={onClick} />
 		</IconTemplate>
 	);
--- src/components/Icons/index.jsx
+++ src/components/Icons/index.jsx
@@ -1,11 +1,25 @@
-import styled from 'styled-components';
+import styled, {css} from 'styled-components';
 
-export default function IconTemplate({children}) {
-	return <StyledIcon>{children}</StyledIcon>;
+export default function IconTemplate({children, variant}) {
+	return <StyledIcon variant={variant}>{children}</StyledIcon>;
 }
 
 const StyledIcon = styled.div`
 	position: absolute;
 	top: 15px;
 	right: 15px;
+	cursor: pointer;
+
+	${({variant}) =>
+		variant === 'close_icon' &&
+		css`
+			padding-top: 5px;
+			padding-left: 5px;
+			padding-right: 4px;
+
+			&:hover {
+				border: 1px solid whitesmoke;
+				border-radius: 50%;
+			}
+		`}
 `;
